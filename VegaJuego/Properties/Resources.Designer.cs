f949ddb0819722387d7e2d40ff5929c2336a120c
El Juanmica <juanmanuel.romero.bermudez@ces-vegamedia.es>
2024-02-20 12:26:40 +0100

    Creacion personaje principal personaje 20-02-2024

--- VegaJuego/Properties/Resources.Designer.cs
+++ VegaJuego/Properties/Resources.Designer.cs
@@ -173,6 +173,16 @@ namespace VegaJuego.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap FondoFinal1 {
+            get {
+                object obj = ResourceManager.GetObject("FondoFinal1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap FondoPantalla {
             get {
                 object obj = ResourceManager.GetObject("FondoPantalla", resourceCulture);
